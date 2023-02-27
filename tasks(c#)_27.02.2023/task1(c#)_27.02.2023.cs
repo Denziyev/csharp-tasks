dceaa675e62cf899e34463515a7d5354bd33017a
İlkin <ilkinhd@code.edu.az>
2023-02-28 01:59:46 +0400

    done succesfully

---
+++ tasks(c#)_27.02.2023/task1(c#)_27.02.2023.cs
@@ -0,0 +1,46 @@
+﻿void searching(int[] array,int number)
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        if (array[i] == number)
+        {
+            Console.WriteLine(number + " exist in array given");
+            break;
+        }
+        else if (i == array.Length - 1)
+        {
+            Console.WriteLine(number + " doesnt exist");
+        }
+    }
+}
+
+
+int[] enterarray()
+{
+    Console.Write("enter the number of elements in array: ");
+    int numbersofelements = int.Parse(Console.ReadLine());
+
+    int[] arr = new int[numbersofelements];
+
+    for (int i = 0; i < numbersofelements; i++)
+    {
+        Console.Write("enter " + (i + 1) + " element: ");
+        int x = int.Parse(Console.ReadLine());
+        arr[i] = x;
+    }
+    
+    return arr;
+}
+
+
+int enternumber()                       
+{
+    Console.Write("Enter the number you search: ");
+    return int.Parse(Console.ReadLine());
+}
+
+
+int[] arr = enterarray();
+int N=enternumber();
+searching( arr, N);
+
---
+++ tasks(c#)_27.02.2023/task2(c#)_27.02.2023.cs
@@ -0,0 +1,13 @@
+﻿int maxvalue(params int[] arr)
+{   int max = arr[0]; 
+    for(int i=1;i<arr.Length;i++)
+    {
+        if (arr[i]>max)
+        { max = arr[i]; }   
+    }
+    return max;
+}
+Console.WriteLine(maxvalue(20, 30, 50, 10, 29, 45,-100));   
+
+
+
